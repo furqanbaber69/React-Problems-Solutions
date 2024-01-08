334ad021bd685887c7939d3ceaeb249ceeb4f5e6
furqan baber <furqanbaber69@gmail.com>
2024-01-08 12:32:47 +0500

    first commit

---
+++ todo/src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from './component/Nav'
+import Text from './component/Text'
+
+
+function App() {
+  
+
+  return (
+  <>
+    <Nav/>
+    <Text/>
+    
+  </>
+  )
+}
+
+export default App
---
+++ todo/src/component/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='Nav-Container'>
+        <div className="logo">
+            <h3>To<span>do</span></h3>
+        </div>
+    <ul className='Nav-Ul'>
+      <li className='Nav-Li'>Home</li>
+      <li className='Nav-Li'>Contact</li>
+      <li className='Nav-Li'>About</li>
+    </ul>
+    
+  </div>
+  )
+}
+
+export default Nav;
